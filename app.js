31af8f34a8f2940005843b32ffb8dbcc40dfb380
Yusuf Kaan <93669959+yusufkkaann@users.noreply.github.com>
2024-05-06 18:19:47 +0300

    ejs modulu kullanilci

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path"); // path modülü şu işe yarar dosya yollarını düzenlemek için kullanılır
+const enj = require("ejs"); // ejs modülü şu işe yarar html dosyalarını düzenlemek için kullanılır
 const app = express();
 const port = 3000;
 
@@ -8,7 +9,18 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   // Bu nedenle, path.resolve() işlevi, dosya yolunu belirtmek için kullanılmaktadır.
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  //res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index.ejs");
+});
+app.get("/about", (req, res) => {
+  // Bu nedenle, path.resolve() işlevi, dosya yolunu belirtmek için kullanılmaktadır.
+  //res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("about.ejs");
+});
+app.get("/contact", (req, res) => {
+  // Bu nedenle, path.resolve() işlevi, dosya yolunu belirtmek için kullanılmaktadır.
+  //res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("contact.ejs");
 });
 
 app.listen(port, () => {
